c08d2338896ff1b4bd8bbf9aea47f24fc6f275af
SathishKumar Loganathan <43195344+SathishKumar-Loganathan@users.noreply.github.com>
2022-04-08 10:45:37 +0530

    Update Test_Simulate_and_Send_Sensor_Readings.cpp

--- Test_Simulate_and_Send_Sensor_Readings.cpp
+++ Test_Simulate_and_Send_Sensor_Readings.cpp
@@ -2,7 +2,13 @@
 #include "Simulate_and_Send_Sensor_Readings.h"
 #include "catch.hpp"
 
-TEST_CASE("Tests to check if data is Simulated and printed correctly") {
-    struct BatteryHealthParameters myBattery;
-    Simulate_and_Send_Sensor_Readings_for_BMS(&myBattery);
+TEST_CASE("Tests to check if float values are Simulated correctly") {
+    int NumberOfReadings = 10;
+    float maximumValue = 50.25;
+    float *BatteryParameterValues;
+    BatteryParameterValues = simulateBatteryParameterFloatValues(NumberOfReadings,maximumValue);
+    
+    for(int i=0; i<NumberOfReadings; i++) {
+        REQUIRE(BatteryParameterValues[i] <= maximumValue);
+    }
 }
