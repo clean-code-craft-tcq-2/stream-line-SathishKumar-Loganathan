f02fbc0a3e2ce3243907e90d15d950d90f83b7e2
SathishKumar Loganathan <43195344+SathishKumar-Loganathan@users.noreply.github.com>
2022-04-20 18:37:16 +0530

    Update Test_Simulate_and_Send_Sensor_Readings.cpp

--- Sender/Test_Simulate_and_Send_Sensor_Readings.cpp
+++ Sender/Test_Simulate_and_Send_Sensor_Readings.cpp
@@ -1,13 +1,11 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "Simulate_and_Send_Sensor_Readings.h"
-#include "catch.hpp"
+#include "../catch.hpp"
 
 TEST_CASE("Tests to check if float values are Simulated correctly") {
     int NumberOfReadings = 10;
     float maximumValue = 50.25;
-    float *BatteryParameterValues;
-    BatteryParameterValues = simulateBatteryParameterFloatValues(NumberOfReadings,maximumValue);
-    
+    float *BatteryParameterValues = simulateBatteryParameterFloatValues(NumberOfReadings,maximumValue);
     for(int i=0; i<NumberOfReadings; i++) {
         REQUIRE(BatteryParameterValues[i] <= maximumValue);
     }
@@ -16,9 +14,7 @@ TEST_CASE("Tests to check if float values are Simulated correctly") {
 TEST_CASE("Tests to check if integer values are Simulated correctly") {
     int NumberOfReadings = 15;
     int maximumValue = 30;
-    int *BatteryParameterValues;
-    BatteryParameterValues = simulateBatteryParameterIntegerValues(NumberOfReadings,maximumValue);
-    
+    int *BatteryParameterValues = simulateBatteryParameterIntegerValues(NumberOfReadings,maximumValue);
     for(int i=0; i<NumberOfReadings; i++) {
         REQUIRE(BatteryParameterValues[i] <= maximumValue);
     }
